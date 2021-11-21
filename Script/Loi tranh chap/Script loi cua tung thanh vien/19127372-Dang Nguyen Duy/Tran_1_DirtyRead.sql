5e95bf2cf1d9a448e8a1bfa856e3ff06eb19638f
Duy Nguyễn <duynguyen1302.bm@gmail.com>
2021-11-21 10:41:47 +0700

    script loi

---
+++ Script/Loi tranh chap/Script loi cua tung thanh vien/19127372-Dang Nguyen Duy/Tran_1_DirtyRead.sql
@@ -0,0 +1,8 @@
+﻿-- T1: Doanh nghiệp cập nhật giá sản phẩm có mã là SP26049509
+-- T2: Khách hàng xem giá sản phẩm có mã là SP26049509
+-- Doanh nghiệp cập nhật giá nhưng không commit => Khách hàng đọc dữ liệu rác
+
+BEGIN TRAN
+	UPDATE dbo.SAN_PHAM SET GIA = 500000 WHERE MA_SP = 'SP26049509'
+	WAITFOR DELAY '00:00:05'
+	ROLLBACK
