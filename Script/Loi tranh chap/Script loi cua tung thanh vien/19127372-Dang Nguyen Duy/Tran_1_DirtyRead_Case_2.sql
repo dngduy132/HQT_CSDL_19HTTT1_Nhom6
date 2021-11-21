ae4d56e372f99aea88446c017f84cdc2be687814
Duy Nguyễn <duynguyen1302.bm@gmail.com>
2021-11-21 13:43:45 +0700

    tranh chap

---
+++ Script/Loi tranh chap/Script loi cua tung thanh vien/19127372-Dang Nguyen Duy/Tran_1_DirtyRead_Case_2.sql
@@ -0,0 +1,8 @@
+﻿-- T1: Doanh nghiệp cập nhật số lượng sản phẩm có mã là SP26049509
+-- T2: Khách hàng xem số lượng sản phẩm có mã là SP26049509
+-- Doanh nghiệp cập nhật số lượng nhưng không commit => Khách hàng đọc dữ liệu rác
+
+BEGIN TRAN
+	UPDATE dbo.SAN_PHAM SET SO_LUONG = 5 WHERE MA_SP = 'SP26049509'
+	WAITFOR DELAY '00:00:05'
+	ROLLBACK
